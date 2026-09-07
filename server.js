c3c1ac04e2b1914faf305776c823a3319912fa8d
Habib Akhtar <habibakhtar593@gmail.com>
2026-09-07 16:59:11 +0100

    /api/register set up and running

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose.connect("mongodb://localhost:27017/authentication-db");
+
 app.use("/", express.static(path.join(__dirname, ".client")));
+app.use(bodyParser.json());
+
+app.post("/api/register", (request, response) => {
+  console.log(request.body);
+});
+
 app.listen(9999, () => {
   console.log("server up at 9999");
 });
